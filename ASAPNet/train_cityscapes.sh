ac3840ea8bc2253ca5bff1b15b9663b3cecca873
Maxim Savinov <cabuhob@gmail.com>
2022-03-19 18:08:39 +0300

    Update ASAPNet

--- ASAPNet/train_cityscapes.sh
+++ ASAPNet/train_cityscapes.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python train.py --name cityscapes_512 --dataroot ../TheCityscapesDataset --dataset_mode cityscapes --batchSize 4 --gpu_ids 0,1 --lr 0.002 --print_freq 500
+python train.py --name cityscapes_512 --dataroot ../TheCityscapesDataset --dataset_mode cityscapes --batchSize 4 --gpu_ids 0,1 --print_freq 500
